09721373ffc8f9eceff630a557fc64a3d8f41be9
twilight <ivampiresp@163.com>
2026-08-20 02:55:52 +0800

    feat: move Error into a shared package

--- assistant/v1/client.gen.go
+++ assistant/v1/client.gen.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"time"
 
+	externalRef0 "github.com/LeaflowNET/leaflow-go/type/v1"
 	"github.com/oapi-codegen/runtime"
 	openapi_types "github.com/oapi-codegen/runtime/types"
 )
@@ -758,12 +759,7 @@ type EarlierResponseBody struct {
 }
 
 // Error defines model for Error.
-type Error struct {
-	Code    *string                `json:"code,omitempty"`
-	Message string                 `json:"message"`
-	Meta    map[string]interface{} `json:"meta,omitempty"`
-	Status  int64                  `json:"status"`
-}
+type Error = externalRef0.Error
 
 // ItemResource defines model for ItemResource.
 type ItemResource struct {
--- compute/v1/client.gen.go
+++ compute/v1/client.gen.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"time"
 
+	externalRef0 "github.com/LeaflowNET/leaflow-go/type/v1"
 	"github.com/oapi-codegen/runtime"
 	openapi_types "github.com/oapi-codegen/runtime/types"
 )
@@ -598,12 +599,7 @@ type DiskTypeResource struct {
 type DiskTypeResourceMedia string
 
 // Error defines model for Error.
-type Error struct {
-	Code    *string                `json:"code,omitempty"`
-	Message string                 `json:"message"`
-	Meta    map[string]interface{} `json:"meta,omitempty"`
-	Status  int64                  `json:"status"`
-}
+type Error = externalRef0.Error
 
 // FloatingIPListResponseBody defines model for FloatingIPListResponseBody.
 type FloatingIPListResponseBody struct {
