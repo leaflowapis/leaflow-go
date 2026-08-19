5f9fee191734464bc98aee7538b05d0d42848b82
twilight <ivampiresp@163.com>
2026-08-20 02:55:06 +0800

    feat: move Error into a shared package

--- iam/v1/client.gen.go
+++ iam/v1/client.gen.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"time"
 
+	externalRef0 "github.com/LeaflowNET/leaflow-go/type/v1"
 	"github.com/oapi-codegen/runtime"
 	openapi_types "github.com/oapi-codegen/runtime/types"
 )
@@ -128,12 +129,7 @@ type CreateSSHKeyRequestBody struct {
 type CreateSSHKeyRequestBodyOwner string
 
 // Error defines model for Error.
-type Error struct {
-	Code    *string                `json:"code,omitempty"`
-	Message string                 `json:"message"`
-	Meta    map[string]interface{} `json:"meta,omitempty"`
-	Status  int64                  `json:"status"`
-}
+type Error = externalRef0.Error
 
 // GrantResource defines model for GrantResource.
 type GrantResource struct {
